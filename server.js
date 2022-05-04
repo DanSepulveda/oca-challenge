7151fc0bdab2777ed2bb3ba21f85fc2ca72df8a8
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-04 14:23:19 -0400

    fixing deploy

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import path from 'path'
+const express = require('express')
+const path = require('path')
 
 const app = express()
 
@@ -12,4 +12,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+app.listen(PORT, '0.0.0.0', () => console.log(`Server listening on port ${PORT}`))
